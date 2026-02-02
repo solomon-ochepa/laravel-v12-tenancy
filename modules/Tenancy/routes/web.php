b99a7b18ad0e5059d76552daf3e1452f4a544bf3
Solomon Ochepa <solomonochepa@gmail.com>
2026-02-02 07:38:31 +0100

    Initialize new Tenancy module
    Scaffold the directory structure and boilerplate files for the new Tenancy module, including controllers, service providers, routes, and resources. Enable the module in modules.json.

---
+++ modules/Tenancy/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Tenancy\App\Http\Controllers\TenancyController;
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('tenancies', TenancyController::class);
+});
