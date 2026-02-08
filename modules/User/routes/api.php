5d5e51ff7735c32957f3f22337cb33e1912220b5
Solomon Ochepa <solomonochepa@gmail.com>
2026-02-08 22:37:06 +0100

    Update tenant user's profile

--- modules/User/routes/api.php
+++ modules/User/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\User\App\Http\Controllers\UserController;
 
 Route::middleware(['auth:api'])->prefix('v1')->group(function () {
-    Route::apiResource('users', UserController::class);
+    //
 });
--- modules/User/routes/tenant/web.php
+++ modules/User/routes/tenant/web.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Modules\User\App\Http\Controllers\Tenant\ProfileController;
 
-Route::middleware(['web', 'tenant'])->group(function () {
-    //
+Route::middleware(['tenant', 'auth', 'verified'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
