8eeeb4da51a553a2232aef88aa8f60b671e4aed8
Solomon Ochepa <solomonochepa@gmail.com>
2026-02-08 15:52:48 +0100

    Configure Tenant Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Nwidart\Modules\Traits\PathNamespace;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    use PathNamespace, WithoutModelEvents;
 
     /**
      * Seed the application's database.
@@ -15,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //
+
+        if (tenant()) {
+            $seeders = array_merge(
+                glob('modules/*/database/seeders/TenantDatabaseSeeder.php'),
+                glob('database/seeders/TenantDatabaseSeeder.php')
+            );
+            $classes = array_map(fn ($path) => $this->path_namespace(str_replace('.php', '', $path)), $seeders);
+
+            $this->call($classes);
+        }
     }
 }
